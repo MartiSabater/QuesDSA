02d5864892d55cb0ed2da346e3ade894b6219cda
Marti <marti.sabater@estudiantat.upc.edu>
2026-03-13 11:35:12 +0100

    Camvis en el Cuaspop

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -3,37 +3,36 @@ public class QueueImpl<E> implements Queue<E>{
     private int p;
 
     public QueueImpl(int len) {
-        // TO-DO
         this.data = (E[])new Object[len];
     }
 
     public void push(E e) throws FullQueueException {
-        // TO-DO
         if (isFull()) throw new FullQueueException();
-
-        this.data[this.p++]=e;
-
+        this.data[this.p++] = e;
     }
 
 
     public E pop() throws EmptyQueueException {
-        // TO-DO
-        return null;
+        if (isEmpty()) throw new EmptyQueueException();
+
+        E value = this.data[0];
+        // Desplaça tots els elements una posició cap a l'esquerra
+        for (int i = 1; i < this.p; i++) {
+            this.data[i - 1] = this.data[i];
+        }
+        this.data[--this.p] = null; // allibera referència per al GC
+        return value;
     }
 
     private boolean isFull() {
-        // TO-DO
-        //return this.p == this.data.length;
-        return false;
+        return this.p == this.data.length;
     }
 
     private boolean isEmpty() {
-        // TO-DO
-        return false;
+        return this.p == 0;
     }
 
     public int size() {
-        //TO-DO
         return this.p;
     }
 }
